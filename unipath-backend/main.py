71f4a0e303813205a07c57ebac5a65563b665628
Mashael Alshathri <MashaelAlshathri1@gmail.com>
2026-08-21 00:13:55 +0300

    Fix CORS for Vercel frontend

--- unipath-backend/main.py
+++ unipath-backend/main.py
@@ -51,7 +51,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://jisr-rho.vercel.app/auth"
+        "https://jisr-rho.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
